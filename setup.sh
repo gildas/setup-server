4b64e0fb6d4145f0b4d02ba8c296a47079676a50
Gildas Cherruel <gildas.cherruel@inin.com>
2014-10-15 11:30:54 +0900

    extracted NIC name

--- setup.sh
+++ setup.sh
@@ -101,8 +101,9 @@ if prompt "The server hostname is: $(hostname), do you want to change it? [yn] "
     if [ "$ID" == "centos" ] ; then
       if [ "$VERSION_ID" == "7" ] ; then
         for config in /etc/sysconfig/network-scripts/ifcfg-* ; do
+          interface="$(basename $config | cut --delimiter=- --fields=2)"
           if [ ! -z "$(grep 'BOOTPROTO="dhcp"' $config)" ] ; then
-            echo "Configuring interface $(basename $config | cut -f- -d1)"
+            echo "Configuring interface $(interface)"
             if [ -z "$(grep DHCP_HOSTNAME $config)" ] ; then
               $NOOP echo "DHCP_HOSTNAME=\"$value\"" | sudo tee --append $config > /dev/null
             else
